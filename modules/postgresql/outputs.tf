5cf82104d5df0c5a487ec8ba1db1076a92b7db1d
Josh Dvir <josh@dvir.us>
2020-06-16 11:54:57 -0400

    fixing outputs

--- modules/postgresql/outputs.tf
+++ modules/postgresql/outputs.tf
@@ -16,47 +16,47 @@
 
 // Master
 output "instance_name" {
-  value       = google_sql_database_instance.default.name
+  value       = google_sql_database_instance.default[0].name
   description = "The instance name for the master instance"
 }
 
 output "public_ip_address" {
   description = "The first public (PRIMARY) IPv4 address assigned for the master instance"
-  value       = google_sql_database_instance.default.public_ip_address
+  value       = google_sql_database_instance.default[0].public_ip_address
 }
 
 output "private_ip_address" {
   description = "The first private (PRIVATE) IPv4 address assigned for the master instance"
-  value       = google_sql_database_instance.default.private_ip_address
+  value       = google_sql_database_instance.default[0].private_ip_address
 }
 
 output "instance_ip_address" {
-  value       = google_sql_database_instance.default.ip_address
+  value       = google_sql_database_instance.default[0].ip_address
   description = "The IPv4 address assigned for the master instance"
 }
 
 output "instance_first_ip_address" {
-  value       = google_sql_database_instance.default.first_ip_address
+  value       = google_sql_database_instance.default[0].first_ip_address
   description = "The first IPv4 address of the addresses assigned."
 }
 
 output "instance_connection_name" {
-  value       = google_sql_database_instance.default.connection_name
+  value       = google_sql_database_instance.default[0].connection_name
   description = "The connection name of the master instance to be used in connection strings"
 }
 
 output "instance_self_link" {
-  value       = google_sql_database_instance.default.self_link
+  value       = google_sql_database_instance.default[0].self_link
   description = "The URI of the master instance"
 }
 
 output "instance_server_ca_cert" {
-  value       = google_sql_database_instance.default.server_ca_cert
+  value       = google_sql_database_instance.default[0].server_ca_cert
   description = "The CA certificate information used to connect to the SQL instance via SSL"
 }
 
 output "instance_service_account_email_address" {
-  value       = google_sql_database_instance.default.service_account_email_address
+  value       = google_sql_database_instance.default[0].service_account_email_address
   description = "The service account email address assigned to the master instance"
 }
 
